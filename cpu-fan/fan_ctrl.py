f897c989a7c708330cffbc2d76275503d7efd91a
Jun (Raspberry Pi 4) <lvjun.summer@live.com>
2021-06-22 22:26:12 +0800

    sync

--- cpu-fan/fan_ctrl.py
+++ cpu-fan/fan_ctrl.py
@@ -24,11 +24,11 @@ FAN_MIN = 0  # [%] Fan minimum speed.
 PWM_FREQ = 100  # [Hz] Change this value if fan has strange behavior
 
 # Configurable temperature and fan speed steps
-tempSteps = [33, 35, 40]  # [°C]
-speedSteps = [73, 73, 100]  # [%]
+tempSteps = [30, 49, 55]  # [°C]
+speedSteps = [0, 0, 100]  # [%]
 
 # Fan speed will change only of the difference of temperature is higher than hysteresis
-hyst = 1
+hyst = 5
 
 # Setup GPIO pin
 GPIO.setmode(GPIO.BCM)
@@ -47,6 +47,10 @@ if len(speedSteps) != len(tempSteps):
     print("Numbers of temp steps and speed steps are different")
     exit(0)
 
+# if the time count exceed the max count that temperature no change (> hyst), maxium the fan speed.
+MAX_COUNT=60 # second
+count=0 # count*WAIT_TIME seconds
+
 try:
     while 1:
         # Read CPU temperature
@@ -57,6 +61,7 @@ try:
         # Calculate desired fan speed
         # logging.debug("New temp %s, old temp %s" % (cpuTemp, cpuTempOld))
         if abs(cpuTemp - cpuTempOld) > hyst:
+            count = 0
             # Below first value, fan will run at min speed.
             if cpuTemp < tempSteps[0]:
                 fanSpeed = speedSteps[0]
@@ -72,17 +77,29 @@ try:
                                          * (cpuTemp - tempSteps[i])
                                          + speedSteps[i], 1)
 
-            #logging.debug("Fan speed %s, old speed %s" % (fanSpeed, fanSpeedOld))
+            logging.debug("Temp %s, Fan speed %s, old speed %s" % (cpuTemp, fanSpeed, fanSpeedOld))
             if fanSpeed != fanSpeedOld:
                 if (fanSpeed != fanSpeedOld
-                        and (fanSpeed >= FAN_MIN or fanSpeed == 0)):
+                   and (fanSpeed >= FAN_MIN or fanSpeed == 0)):
                     fan.ChangeDutyCycle(fanSpeed)
                     logging.info("Fan on @ %s, fan speed= %.2f" % (cpuTemp, fanSpeed))
                     fanSpeedOld = fanSpeed
             cpuTempOld = cpuTemp
+        else:
+            count = count + 1
+            if (count > MAX_COUNT) and (fanSpeed != 0):
+               fanSpeed = speedSteps[len(tempSteps) - 1]
+               fan.ChangeDutyCycle(fanSpeed)
+               logging.info("Fan on @ %s, long time no change, fan speed = %.2f" % (cpuTemp, fanSpeed))
+               fanSpeedOld = fanSpeed
+               cpuTempOld = cpuTemp #check the temp against current but not the last time, this will force the temp reduce <hyst> value
+               count = 0
+            else:
+               pass
 
         # Wait until next refresh
         time.sleep(WAIT_TIME)
+
 
 
 finally:
